b07c125e717a5c23ac8f28086921d0b792f9a31c
Dominik <gilges.dominik@gmail.com>
2026-01-29 12:44:01 +0100

    landing page: applied styling to the landing page

--- src/app/landing-component/landing-component.ts
+++ src/app/landing-component/landing-component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 import { SessionsService } from '../sessions-service';
 import { NewSessionComponent } from './new-session-component/new-session-component';
 
 @Component({
   selector: 'app-landing-component',
-  imports: [RouterLink, MatButtonModule, NewSessionComponent],
+  imports: [RouterLink, MatButtonModule, MatCardModule, NewSessionComponent],
   templateUrl: './landing-component.html',
   styleUrl: './landing-component.css',
 })
